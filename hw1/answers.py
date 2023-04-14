7ba7d44e0ab23a6e4a5612e48d3410f3b9a35f7a
matansol <matan.solomo@gmail.com>
2023-04-14 16:12:39 +0300

    done

--- hw1/answers.py
+++ hw1/answers.py
@@ -11,35 +11,23 @@ math (delimited with $$).
 part1_q1 = r"""
 **Your answer:**
 
-1. false, we train our model on the training set and there can be a case the train and test sets has distribution differences so the test error is diferent from the train error = in-sample error.
+1. **false**, we train our model on the training set and there can be a case the train and test sets has distribution differences so the test error is diferent from the train error = in-sample error.
 
-2. false, for example if we are in classification problem with 2 classe {1,-1} if we split the data that the train will be all the samples with lable 1 we will get a tilted model that give 1 to all the samples when its not the case.
+2. **false**, for example if we are in classification problem with 2 classe {1,-1} if we split the data that the train will be all the samples with lable 1 we will get a tilted model that give 1 to all the samples when its not the case.
 
-3. true, the cross validation is part of the learning rate so if we used the test set than the model is relaying on the test set, so the test set is no longer valid for testing.
+3. **true**, the cross validation is part of the training part so if we used CV with the test set than the model training is relaying on the test set, so the test set is no longer valid for testing.
 
-4. false, after the cross validation we train the model on all the data with optimal hyperparameter and then we use the test set to get the proxy generalization error.
+4. **false**, after the cross validation we train the model on all the data with optimal hyperparameter and then we use the test set to get the proxy generalization error.
 
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
 
 """
 
 part1_q2 = r"""
 **Your answer:**
 
-No, our friend approch is not justified,
+**No, our friend approch is not justified**,
 we can't use the test set to tune hyperparameters, for that we have the validation set. when we using the test set to choose the optimal $\lambda$ the test set is no longer valid as an proxy for the generalization error.
 
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 1$
 
 """
 
@@ -49,14 +37,9 @@ An equation: $e^{i\pi} -1 = 1$
 part2_q1 = r"""
 **Your answer:**
  
-not necessarily, when k is to big (for example when k = m (num of samples)) the model predict each point to labled as the most common lable from all the samples, and its will let to poor accuracy. we can use cross-validation to get the best k.
+**not necessarily**, when k is to big (for example when k = m (num of samples)) the model predict each point to labled as the most common lable from all the samples, and its will let to poor accuracy. we can use cross-validation to get the best k.
 
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
 ```
-An equation: $e^{i\pi} -1 = 0$
 
 """
 
@@ -64,14 +47,8 @@ part2_q2 = r"""
 **Your answer:**
 1. in order to estimate the generalization error we need to run the model on samples that the model hasn't see before, if we will find the parameters values based on the all training samples the optimal k will be 1 and we will get 100% accuracy because each dot is the closes neighbor of herself and that is not the case on unseen data. 
 
-2. by choosing the best model based on the test accuracy we useing the test set as part of our training so the model is now tilted to fit the test set and we can not use the set test as an andipended set in order to astimate the generalization error. 
+2. by choosing the best model based on the test accuracy we useing the test set as part of our training, so the model is now tilted to fit the test set and we can not use the set test as an andipended set in order to astimate the generalization error. 
 
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
 
 """
 
@@ -87,12 +64,6 @@ If we change the value of  $\Delta$ , we will need to change our *$W$* weights m
 predictions depend on the boundaries that we get, therefore the value of $\Delta$ is arbitrary.
 
 
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
 
 """
 
@@ -106,18 +77,12 @@ As we can see in the example above, if in the data test there are some digits th
 The difference between this model to the KNN- model is that when the model make the predictions on the data test,
 the result in soft svm depends on all the data compared to in KNN that the result depends on only the k- cloests samples.
 
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
 
 """
 
 part3_q3 = r"""
 **Your answer:**
-We think that the learning rate that we chose is good.
+We think that the learning rate that we chose is **good**.
 First, we can see that the loss-graph that we get is decreased almost monotonically and rapidly.
 Second, we can assume that if the value of the learning rate was too high so we would get a graph with divergent behavior while jumping over the minimum without converging to it.
 And if the value of the learning rate was too low, we would get a graph that has slowly progressing to the minimum, and should have a much bigger number of epoch in order to converge.
@@ -127,12 +92,6 @@ As we can see in the graphs above, the loss function of the traning is smaller t
 Also in the accuracy graph of the traning we can see that the accuracy still increase while the accuracy of the validation remains unchanged.
 The gap between the traning and the validation is not so bad but maybe with another data test we can get much huge gap that can point on overfitting.
 
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
 
 """
 
@@ -148,12 +107,6 @@ the optimal pattern is that all the dots are on the 'y=0' line because that mean
 we can see from the plots that most of the dots are close to 0 means the model prediction was good, but some dots have a big error - after the CV the number of dots with
 big error is decreas and the dots are closer to 0 as axpected.
 
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
 
 """
 
@@ -161,19 +114,13 @@ part4_q2 = r"""
 **Your answer:**
 1. It is still a linear model because the linerity of the model comes from that the liniar multipy of W in X and adding more columns in X doesn't effect the linear multipy.
 
-2. Yes we can, the a simple example is on 2-D with dots in distance 1 and 2 from center (0,0), dots with distance 1 gets the lable 1 and dots with distance 2 get the lable -1. 
+2. **Yes we can**, the a simple example is on 2-D with dots in distance 1 and 2 from center (0,0), dots with distance 1 gets the lable 1 and dots with distance 2 get the lable -1. 
    without more feature the model can't separate the dots but if we will add the fature $x_1^2 + x_2^2$ the model could split the data with the line y = 3 for example.
    
 3. if we are adding a non-linear features to our data, we will get that the decision boundary still be a hyperplane, but because the new features are non-linear, we can assume by looking them as more features in higher dimension.
 So, we will get a new hyperplane which will separate our data in this higher space.
 As a result, the new decision boundary probably will not be linear and because of that will not be a hyperplane in the orginial space.
 
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
 
 """
 
@@ -184,13 +131,13 @@ part4_q3 = r"""
 2. the $\lambda$ range has 20 different values, the degree range has 3 and k_fold is set to 3.
    so its total of 180 fits.    
 
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
 
 """
 
 # ==============
+# Write your answer using **markdown** and $\LaTeX$:
+# ```python
+# # A code block
+# a = 2
+# ```
+# An equation: $e^{i\pi} -1 = 0$
